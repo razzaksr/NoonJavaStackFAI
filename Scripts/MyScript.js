9d76e09c43ea476640cb267da94386de134c9160
razzaksr <razzaksr@gmail.com>
2022-05-26 16:00:51 +0530

    array filter

--- Scripts/MyScript.js
+++ Scripts/MyScript.js
@@ -19,6 +19,14 @@ var myArray=[
     }
 ]
 
+const deleting=(index)=>{
+    alert(index+" caught @ delete ")
+    myArray=myArray.filter((ele,pos)=>{
+        return pos!==index
+    })
+    listing()
+}
+
 const listing=()=>{
     var data=""
     
@@ -28,6 +36,7 @@ const listing=()=>{
         data+="<td>"+ele.exp+"</td>"
         data+="<td>"+ele.current+"</td>"
         data+="<td>"+ele.status+"</td>"
+        data+="<td><button class='btn btn-outline-danger' onclick='deleting("+pos+")'>Delete</button></td>"
         data+="</tr>"
     })
     
